6a05038d52482bcd3af5ab24cb979559f2d2a57e
ashu.gajare <ashu.gajare@gmail.com>
2026-05-26 22:30:39 +0530

    ci: fix allure reporter installation and directory paths

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html',
   //Adding Allure configuration 
-  reporter: [['line'],['allure-playwright',{resultsDir:'allure-result'}]],
+  reporter: [['line'],['allure-playwright',{resultsDir:'allure-results',         
+        detail: true, 
+        attachments: true, 
+        suiteTitle: true}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
